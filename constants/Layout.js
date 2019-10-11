733af884434060f1ee4bd1b492eb502ec331dc9a
Simon <greenspanx@hotmail.com>
2019-10-11 15:08:51 +0800

    Layout

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,4 +1,4 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
@@ -6,7 +6,19 @@ const height = Dimensions.get('window').height;
 export default {
   window: {
     width,
-    height,
+    height
   },
-  isSmallDevice: width < 375,
+  isSmallDevice: width < 375
 };
+
+export function isIphoneX() {
+  const dimen = Dimensions.get('window');
+  return (
+    Platform.OS === 'ios' &&
+    !Platform.isPad &&
+    !Platform.isTVOS &&
+    (dimen.height === 812 ||
+      dimen.width === 812 ||
+      (dimen.height === 896 || dimen.width === 896))
+  );
+}
