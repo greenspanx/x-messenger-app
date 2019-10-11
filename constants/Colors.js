030d14e5412aa4e8ec35b34016bbaa5c3d484433
Simon <greenspanx@hotmail.com>
2019-10-11 15:09:54 +0800

    Colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,37 @@
-const tintColor = '#2f95dc';
+const tintColor = '#5a5a5a';
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
+  tabIconDefault: '#b2b2b2',
   tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+  primary: '#528bcc',
+  secondary: '#efefef',
+  danger: '#ff314c',
+  text: '#525252'
+};
+
+export const tabBarOptions = {
+  style: {
+    shadowColor: 'rgb(46, 61, 73)',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.1,
+    shadowRadius: 7,
+    borderTopWidth: 0,
+    elevation: 20
+  },
+  showLabel: false
+};
+
+export const navigatorOptions = {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: '#fff',
+      borderBottomWidth: 0,
+      shadowColor: '#2e3d4980',
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.1,
+      shadowRadius: 7
+    },
+    headerTintColor: '#2d2d2d'
+  }
 };
